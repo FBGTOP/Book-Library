89522c126954947609b7cae445c567e2ffa22bbf
FBGTOP <139357201+FBGTOP@users.noreply.github.com>
2023-11-05 22:05:36 +0000

    Implement add and remove book;
    Code for ading new book to the main container.
    Also add buttons for removing book and read.
    No ocding behind read button yet.

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 let library = [];
 
-function Book() {
-
+function Book(title, author, pages, rating) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.rating = rating;
 }
 
-function addBook(title) {
-    library.push(title);
+function addBook(title, author, pages, rating) {
+    library.push(new Book (title, author, pages, rating));
+    display(library);
 }
 
-function display(array){
-    array.forEach(element => {
-        const container = document.querySelector(".bookContainer");
+function removeBook(target){
+    const book = document.getElementById(`${target}`).parentNode;
+    book.remove();
+    let index = library.findIndex(index => index.title === `${target}`);
+    if(index === -1){
+        return
+    }
+    library.splice(index, 1);
+}
 
-        const book = document.createElement("div");
-        book.classList.toggle('book');
-        book.textContent = `${element}`;
+function display(bookArray){
 
-        container.appendChild(book);
+    const container = document.querySelector(".bookContainer");
+    const book = document.createElement("div");
+    const bookTitle = document.createElement("div");
+    const bookAuthor = document.createElement("div");
+    const bookPages = document.createElement("div");
+    const bookRating = document.createElement("div");
+    const removeBtn = document.createElement ("button");
+    const readBtn = document.createElement ("button")
+    removeBtn.addEventListener("click", (event)=>{
+        removeBook(event.target.id);
     });
+
+    bookArray.forEach(element => {
+
+        book.classList.add("book");
+        bookTitle.classList.add("bookTitle");
+        bookAuthor.classList.add('bookAuthor');
+        bookPages.classList.add("bookPages");
+        bookRating.classList.add("bookRating");
+        removeBtn.classList.add("removeBtn");
+        readBtn.classList.add("readBtn");
+        removeBtn.setAttribute("id", `${element.title}`);
+
+
+        bookTitle.textContent = `${element.title}`;
+        bookAuthor.textContent = `${element.author}`;
+        bookPages.textContent = `${element.pages}`;
+        bookRating.textContent = `${element.rating}`;
+        
+
+        book.append(bookTitle, bookAuthor, bookPages,bookRating, removeBtn, readBtn);
+        container.append(book);
+    });
+
 }
 
+//Dialog box activation
 const bookForm = document.querySelector("dialog");
 const showAddBook = document.getElementById("addBookBtn");
 const closeAddBook = document.querySelector("dialog button")
@@ -29,13 +70,16 @@ showAddBook.addEventListener("click", () => {
 }
 )
 
-closeAddBook.addEventListener("click", () => {
+//Collect form data to object
+
+const addTitle = document.querySelector("#title");
+const addAuthor = document.querySelector("#author");
+const addPages = document.querySelector("#pages");
+const addRating = document.querySelector("#rating");
+const bookSubmit = document.querySelector("#submit")
+
+bookSubmit.addEventListener('click', (event) => {
+    addBook(addTitle.value, addAuthor.value, addPages.value, addRating.value);
     bookForm.close();
-})
+});
 
-
-
-addBook("The Hobbit");
-addBook("Private Peaceful")
-addBook("Something Something")
-display(library);
